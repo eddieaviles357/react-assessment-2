59ebe3923d6b181248204e09d24cc72cd3c066bf
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-28 12:56:09 -0700

    before and after test added

--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -1,5 +1,18 @@
 const unroll = require("./unroll");
 
+let square = null;
+beforeAll(function() {
+  square = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+  ];
+})
+
+afterAll(function() {
+  square = null;
+})
 describe("#unroll", function () {
 
   it("is a function", function () {
