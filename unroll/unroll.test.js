8197b99ebd2e27cbf84fb6a8888c4189c7481b4a
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-28 13:11:26 -0700

    added one more test

--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -1,6 +1,7 @@
 const unroll = require("./unroll");
 
 let square = null;
+let smallerSquare = null;
 beforeAll(function() {
   square = [
     [1, 2, 3, 4],
@@ -8,10 +9,16 @@ beforeAll(function() {
     [9, 10, 11, 12],
     [13, 14, 15, 16]
   ];
+  smallerSquare = [
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+    ["g", "h", "i"]
+  ];
 })
 
 afterAll(function() {
   square = null;
+  smallerSquare = null;
 })
 describe("#unroll", function () {
 
@@ -24,5 +31,7 @@ describe("#unroll", function () {
 describe('should return a single array with values', function () {
   it('should return return values in a spiral', function () {
     expect(unroll(square)).toEqual([1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]);
-  })
+    expect(unroll(smallerSquare)).toEqual(["a", "b", "c", "f", "i", "h", "g", "d", "e"]);
+  });
+  
 })
