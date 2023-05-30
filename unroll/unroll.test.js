395100d4c91e0e143736183fad4aa97495db0d93
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-29 20:48:58 -0700

    added fail test

--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -2,6 +2,7 @@ const unroll = require("./unroll");
 
 let square = null;
 let smallerSquare = null;
+let notSquare = null;
 beforeAll(function() {
   square = [
     [1, 2, 3, 4],
@@ -14,12 +15,18 @@ beforeAll(function() {
     ["d", "e", "f"],
     ["g", "h", "i"]
   ];
+  notSquare = [
+    [1,2,3],
+    [4,5,6]
+  ];
 })
 
 afterAll(function() {
   square = null;
   smallerSquare = null;
+  notSquare = null;
 })
+
 describe("#unroll", function () {
 
   it("is a function", function () {
@@ -33,5 +40,10 @@ describe('should return a single array with values', function () {
     expect(unroll(square)).toEqual([1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]);
     expect(unroll(smallerSquare)).toEqual(["a", "b", "c", "f", "i", "h", "g", "d", "e"]);
   });
-  
+});
+
+describe('should return -1 if not a square', function () {
+  it('should return -1', function() {
+    expect(unroll([[1,2,3],[4,5,6]])).toEqual(-1);
+  })
 })
