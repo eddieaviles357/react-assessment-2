c4f0b3a604bf1f4fd6ca112815e1e69fe0639321
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-26 20:43:57 -0700

    Routes moved to segregates from app

--- snack-or-booze/src/Routes.js
+++ snack-or-booze/src/Routes.js
@@ -1,39 +1,31 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from "./Home";
-import NavBar from "./NavBar";
 import Menu from "./FoodMenu";
 import Snack from "./FoodItem";
 
-export default function Routes({snacks, drinks}) {
+
+export default function Main({snacks, drinks}) {
     return (
-    <BrowserRouter>
-        <NavBar />
-        <main>
-          <Switch>
+        <Switch>
             <Route exact path="/">
-              <Home snacks={snacks} />
+                <Home snacks={snacks} />
             </Route>
             <Route exact path="/snacks">
-              <Menu snacks={snacks} title="Snacks" />
+                <Menu snacks={snacks} title="Snacks" />
             </Route>
             <Route path="/snacks/:id">
-              <Snack items={snacks} cantFind="/snacks" />
+                <Snack items={snacks} cantFind="/snacks" />
             </Route>
-
             <Route exact path="/drinks">
-              <Menu snacks={drinks} title="Drinks" />
+                <Menu snacks={drinks} title="Drinks" />
             </Route>
             <Route path="/drinks/:id">
-              <Snack items={drinks} cantFind="/drinks" />
+                <Snack items={drinks} cantFind="/drinks" />
             </Route>
-
-
             <Route>
-              <p>Hmmm. I can't seem to find what you want.</p>
+                <p>Hmmm. I can't seem to find what you want.</p>
             </Route>
-          </Switch>
-        </main>
-      </BrowserRouter>
+        </Switch>
     )
-}
+};
