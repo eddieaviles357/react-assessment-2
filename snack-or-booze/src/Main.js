d85db092065fbeb37ddb1a4be1841cb2eee64292
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-26 20:46:22 -0700

    imported Routes and added props drinks and snacks coming from parent component

--- snack-or-booze/src/Main.js
+++ snack-or-booze/src/Main.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import NavBar from "./NavBar";
+import Routes from "./Routes";
 
 export default function Main({snacks, drinks}) {
     return (
     <BrowserRouter>
         <NavBar />
         <main>
-
+          <Routes snacks={snacks} drinks={drinks} />
         </main>
       </BrowserRouter>
     )
