1901250152a97554425352af6042c66882f46754
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-26 20:37:50 -0700

    moved all Routes to its own component

--- snack-or-booze/src/App.js
+++ snack-or-booze/src/App.js
@@ -1,12 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter } from "react-router-dom";
+import Routes from './Routes';
 import "./App.css";
-import Home from "./Home";
 import SnackOrBoozeApi from "./Api";
-import NavBar from "./NavBar";
-import { Route, Switch } from "react-router-dom";
-import Menu from "./FoodMenu";
-import Snack from "./FoodItem";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -30,34 +25,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <NavBar />
-        <main>
-          <Switch>
-            <Route exact path="/">
-              <Home snacks={snacks} />
-            </Route>
-            <Route exact path="/snacks">
-              <Menu snacks={snacks} title="Snacks" />
-            </Route>
-            <Route path="/snacks/:id">
-              <Snack items={snacks} cantFind="/snacks" />
-            </Route>
-
-            <Route exact path="/drinks">
-              <Menu snacks={drinks} title="Drinks" />
-            </Route>
-            <Route path="/drinks/:id">
-              <Snack items={drinks} cantFind="/drinks" />
-            </Route>
-
-
-            <Route>
-              <p>Hmmm. I can't seem to find what you want.</p>
-            </Route>
-          </Switch>
-        </main>
-      </BrowserRouter>
+    <Routes snacks={snacks} drinks={drinks}/>
     </div>
   );
 }
