d02fb5b86e159152e8fb0819a868634295b6c296
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-26 20:44:13 -0700

    moved routes to its own file

---
+++ snack-or-booze/src/Main.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import Menu from "./FoodMenu";
+import Snack from "./FoodItem";
+
+export default function Main({snacks, drinks}) {
+    return (
+    <BrowserRouter>
+        <NavBar />
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <Home snacks={snacks} />
+            </Route>
+            <Route exact path="/snacks">
+              <Menu snacks={snacks} title="Snacks" />
+            </Route>
+            <Route path="/snacks/:id">
+              <Snack items={snacks} cantFind="/snacks" />
+            </Route>
+
+            <Route exact path="/drinks">
+              <Menu snacks={drinks} title="Drinks" />
+            </Route>
+            <Route path="/drinks/:id">
+              <Snack items={drinks} cantFind="/drinks" />
+            </Route>
+
+
+            <Route>
+              <p>Hmmm. I can't seem to find what you want.</p>
+            </Route>
+          </Switch>
+        </main>
+      </BrowserRouter>
+    )
+}
