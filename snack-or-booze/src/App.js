11062047ba01bc5489daf1aa23bf37bfecaf0970
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-06-27 20:55:00 -0700

    addItem was added to our context provider, also name is slugified

--- snack-or-booze/src/App.js
+++ snack-or-booze/src/App.js
@@ -3,6 +3,7 @@ import Main from './Main';
 import "./App.css";
 import SnackOrBoozeApi from "./Api";
 import SnacksDrinksContext from "./SnacksDrinksContext";
+import slugify from "slugify";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -19,7 +20,17 @@ function App() {
       setIsLoading(false);
     }
     getSnacks();
-  }, []);
+  }, [isLoading]);
+
+  async function addItem({ type, name, description, recipe, serve }) {
+    // lower case name as we are going to use this as the id
+    // slugify also will replace a space with -. Eg( Big Car -> big-car )
+    let id = slugify(name, { lower: true });
+    let item = { id, name, description, recipe, serve };
+    await SnackOrBoozeApi.addItem(type, item);
+    // trigger useEffect to rerender and update data in db
+    setIsLoading(true)
+  }
 
   if (isLoading) {
     return <p>Loading &hellip;</p>;
@@ -27,7 +38,7 @@ function App() {
 
   return (
     <div className="App">
-    <SnacksDrinksContext.Provider value={{snacks, drinks}}>
+    <SnacksDrinksContext.Provider value={{snacks, drinks, addItem}}>
       <Main />
     </SnacksDrinksContext.Provider>
     </div>
